6a23fe95734d084f6dca1dc5a69f7c0df28c6c2c
LeonardoRomulo <leo.romulo15@gmail.com>
2025-04-07 16:59:49 -0300

    feature: subindo o site de uma barber_shop

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,40 @@
+'use client';
+import { useState } from 'react';
 import Image from "next/image";
-import styles from "./page.module.css";
+import estilos from "./page.module.css";
+import { BsSun, BsMoon } from "react-icons/bs";
+import Logo from '../../public/barbearia.png';
+import Banner from '../../public/banner.jpg'
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [darkTheme, setDarkTheme] = useState(false);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  function mudarTema() {
+    setDarkTheme(!darkTheme);
+  }
+
+  return (
+    <div className={darkTheme ? estilos.dark : estilos.light}>
+    <main className={estilos.main}>
+      <header className={estilos.topo}>
+        <div className={`${estilos.container} ${estilos.limitar_secao}`}>
+          <Image className={estilos.image} src={Logo} alt="logo barbearia"></Image>
+          <button onClick={mudarTema}> {darkTheme ? <BsSun /> : <BsMoon />}</button>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </header>
+
+      <section className={estilos.secao_banner}>
+      </section>
+
+      <section className={estilos.container_texto}>
+        <div className={estilos.secao_texto}>
+        <h1>Bem-Vindo a Barber Shop</h1>
+        <p>Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas.</p>
+        <p>Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos uma equipe premiada que demonstrou o talento de mestres barbeiros em vários concursos de estilo. Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado.</p>
+        <p>S.Kelly</p>
+        </div>
+      </section>
+    </main>
     </div>
   );
 }
