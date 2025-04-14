68deef9b4a41c8dcddb71088a0412a71debe4bb3
LeonardoRomulo <leo.romulo15@gmail.com>
2025-04-14 15:11:01 -0300

    feat: alterando o estilo e adicionando icones ao componente card com props

---
+++ src/app/componentes/Topo/index.jsx
@@ -0,0 +1,23 @@
+import estilos from "./Topo.module.css";
+import Image from "next/image";
+import { BsSun, BsMoon } from "react-icons/bs";
+import Logo from "../../../../public/barbearia.png";
+
+export default function Topo({alterarTema, darkTheme}) {
+  return (
+    <header className={estilos.topo}>
+      <div className={`${estilos.container} ${estilos.limitar_secao}`}>
+        <Image
+          className={estilos.imagem}
+          src={Logo}
+          alt="logo barbearia"
+        ></Image>
+        <button 
+        onClick={() => alterarTema(darkTheme)}
+            >
+          {darkTheme ? <BsSun /> : <BsMoon />}
+        </button>
+      </div>
+    </header>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-'use client';
-import { useState } from 'react';
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 import estilos from "./page.module.css";
-import { BsSun, BsMoon } from "react-icons/bs";
-import Logo from '../../public/barbearia.png';
-import Banner from '../../public/banner.jpg'
+
+import Banner from "../../public/banner.jpg";
+import Topo from "./componentes/Topo";
 
 export default function Home() {
   const [darkTheme, setDarkTheme] = useState(false);
@@ -15,26 +15,27 @@ export default function Home() {
 
   return (
     <div className={darkTheme ? estilos.dark : estilos.light}>
-    <main className={estilos.main}>
-      <header className={estilos.topo}>
-        <div className={`${estilos.container} ${estilos.limitar_secao}`}>
-          <Image className={estilos.image} src={Logo} alt="logo barbearia"></Image>
-          <button onClick={mudarTema}> {darkTheme ? <BsSun /> : <BsMoon />}</button>
-        </div>
-      </header>
+      <main className={estilos.main}>
+        <Topo alterarTema={mudarTema} dark={darkTheme} />
+        <section className={estilos.secao_banner}></section>
 
-      <section className={estilos.secao_banner}>
-      </section>
-
-      <section className={estilos.container_texto}>
-        <div className={estilos.secao_texto}>
-        <h1>Bem-Vindo a Barber Shop</h1>
-        <p>Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas.</p>
-        <p>Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos uma equipe premiada que demonstrou o talento de mestres barbeiros em vários concursos de estilo. Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado.</p>
-        <p>S.Kelly</p>
-        </div>
-      </section>
-    </main>
+        <section className={estilos.container_texto}>
+          <div className={estilos.secao_texto}>
+            <h1>Bem-Vindo a Barber Shop</h1>
+            <p>
+              Nossa barbearia sempre oferece profissionais de qualidade e
+              estamos prontos para lidar com suas maiores expectativas.
+            </p>
+            <p>
+              Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos
+              uma equipe premiada que demonstrou o talento de mestres barbeiros
+              em vários concursos de estilo. Deixe nosso barbeiro ser seu
+              estilista pessoal e você nunca ficará desapontado.
+            </p>
+            <p>S.Kelly</p>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
